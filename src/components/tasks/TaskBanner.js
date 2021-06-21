ce6aba3daaafea29e10186b0f723e96d26cab946
JeremiasA <jeremiasamestoy@gmail.com>
2021-06-21 04:43:34 -0300

    menu fixes

--- src/components/tasks/TaskBanner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './styles.css'
-
-export const TaskBanner = props =>{
-    return <h3 className="bannerStyle bg-primary text-white text-center p.4">
-       <p style={{textAlign:"left", marginLeft:"5px"}}>{props.user} Task App [
-           {props.taskItems.filter(task => !task.done && task.user === props.user).length} task to done]</p>
-    </h3>
-}
