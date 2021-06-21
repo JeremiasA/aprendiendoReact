5c4fd8910db8373be115b10ceddf0a4b972d4a94
JeremiasA <jeremiasamestoy@gmail.com>
2021-06-21 04:42:15 -0300

    menu and basic login component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { TaskRow } from "./components/tasks/TaskRow";
-import { TaskBanner } from './components/tasks/TaskBanner'
+import { Menu } from './components/menu/Menu'
+import { Footer } from './components/footer/Footer'
 import { TaskCreator } from './components/tasks/TaskCreator'
 import { UserRow } from './components/users/userRow'
 import {deleteConfirm, deletedAlert} from './alerts/alerts'
 import './App.css'
-
+import SignIn from './pages/SinginForm'
 
 function App() {
     
@@ -13,21 +14,32 @@ function App() {
         {
             username: "Jeremias",
             active: true,
-            deletionConfirm: true
+            deletionConfirm: true,
+            role:"regular"
         },
         {
             username: "Karla",
             active: false,
-            deletionConfirm: true
+            deletionConfirm: true,
+            role:"regular"
         },
         {
             username: "Abril",
             active: false, 
-            deletionConfirm: true
+            deletionConfirm: true,
+            role:"regular"
+        },
+        {
+            username: "Admin",
+            active: false,
+            logged: true, 
+            deletionConfirm: true,
+            role:"admin"
         }
     ]);
 
     const [activeUser, setActiveUser] = useState(users.find(u => u.active));
+    const [loggedUser, setLoggedUser] = useState(users.find(u => u.logged));
     const [taskItems, setTaskItems] = useState([
         { name: "task one", user: "Jeremias",  done: false, id:1 },
         { name: "task two", user:"Karla", done: false, id:2 },
@@ -116,47 +128,98 @@ function App() {
             
         }
         }   
-        
-        return (
-        <div>
-            <TaskBanner user={activeUser.username} taskItems={taskItems} />
-            <hr />
-            <TaskCreator
-                cb={addNewTask}
-                users={users}
-                tasksLength={taskItems.length}
-            />
-            <hr />
-            <div className="divTablesStyle">
-                <table className="tableStyle table table-striped table-bordered">
-                    <thead className="thStyle">
-                        <tr>
-                            <th className="thStyle text-center">Description</th>
-                            <th style={{width:"10px"}} className="thStyle text-center">Done</th>
-                            <th style={{width:"10px"}} className="thStyle text-center">Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>{taskTableRows()}</tbody>
-                </table>
-                <table style={{marginLeft:"20px"}}>
-                    <tr>
-                        <td className="bg-primary" width="2"></td>
-                    </tr>
-                </table>
-                <table
-                    className="usersTableStyle table table-striped table-bordered"
-                >
-                    <thead>
-                        <tr>
-                            <th>User</th>
-                            <th>Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>{userTableRows()}</tbody>
-                </table>
-            </div>
-        </div>
-    );
-}
+       
+        const [signed, setSigned] = useState(false)
 
-export default App;
+        
+        const leerData = ({pass, username}) =>{
+            
+            // BUSCAR Y VALIDAR CLAVE Y USUARIO
+            console.log("pass", pass)
+            let clave = "1234"
+            if(pass===clave)
+            setSigned(true)
+        }
+  
+
+        if(signed === false){
+            return(<SignIn cb={leerData} />)
+        } else {
+
+            
+                    if(loggedUser.role==="admin"){
+                        return (
+                            <div>
+                                <Menu />
+                                <TaskCreator
+                                    cb={addNewTask}
+                                    users={users}
+                                    tasksLength={taskItems.length}
+                                />
+                                <hr />
+                                <div className="divTablesStyle">
+                                    <table className="tableStyle table table-striped table-bordered">
+                                        <thead className="thStyle">
+                                            <tr>
+                                                <th className="thStyle text-center">Description</th>
+                                                <th style={{width:"10px"}} className="thStyle text-center">Done</th>
+                                                <th style={{width:"10px"}} className="thStyle text-center">Delete</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>{taskTableRows()}</tbody>
+                                    </table>
+                                    <table style={{marginLeft:"20px"}}>
+                                        <tr>
+                                            <td className="bg-primary" width="2"></td>
+                                        </tr>
+                                    </table>
+                    
+                                    <table
+                                        className="usersTableStyle table table-striped table-bordered"
+                                    >
+                                        <thead>
+                                            <tr>
+                                                <th>User</th>
+                                                <th>Tasks</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>{userTableRows()}</tbody>
+                                    </table>
+                                </div>
+                            <Footer user={loggedUser.username} taskItems={taskItems} />
+                            </div>
+                    
+                        )
+                    } else{
+                            return (
+                                    <div>
+                                        <Menu />
+                                        <hr />
+                                        <div className="divTablesStyle">
+                                            <table className="tableStyle table table-striped table-bordered">
+                                                <thead className="thStyle">
+                                                    <tr>
+                                                        <th className="thStyle text-center">Description</th>
+                                                        <th style={{width:"10px"}} className="thStyle text-center">Done</th>
+                                                        <th style={{width:"10px"}} className="thStyle text-center">Delete</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>{taskTableRows()}</tbody>
+                                            </table>
+                                            <table style={{marginLeft:"20px"}}>
+                                                <tr>
+                                                    <td className="bg-primary" width="2"></td>
+                                                </tr>
+                                            </table>
+                            
+                            
+                                        </div>
+                                    <Footer user={activeUser.username} taskItems={taskItems} />
+                                    </div>
+                                )
+                            };
+                        }
+                        }
+                        
+                        export default App;
+                        
